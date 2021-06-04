f7ce33e471f72c86dd7ceee5f285bc6c924dc1e5
Emily Easteal <emily@easteal.dev>
2021-06-04 15:24:57 +0100

    cleaner code using imports and better code practise for naming

---
+++ plover_minimal_english_stenotype.py
@@ -0,0 +1,9 @@
+from plover.system.english_stenotype import *
+
+
+# Remove all the folding orthography stuff.
+
+SUFFIX_KEYS = ()
+ORTHOGRAPHY_RULES = []
+ORTHOGRAPHY_WORDLIST = None
+ORTHOGRAPHY_RULES_ALIASES = {}
